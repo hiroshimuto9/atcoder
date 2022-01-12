e3a8dd7f6d19714636d9bd5941fb93bc4690daca
mutohiroshi <mutohiroshi@team-lab.com>
2022-01-12 20:39:18 +0900

    fix: ABC098のA問題を実装

--- src/ABC/098/A.ts
+++ src/ABC/098/A.ts
@@ -1,4 +1,6 @@
 export const A_098 = (input: string) => {
-  const N = +input.trim();
-  console.log(N-1);
+  const [A, B]= [...input.trim().split(' ').map(Number)];
+  let max = Math.max(A-B, A*B, A+B);
+  if(max === 0) max = 0
+  console.log(max);
 }
